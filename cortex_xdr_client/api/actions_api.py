338e6b39c0d804c2480e9622a71c4bef472e8583
lawiet <rustam.shirinov@glovoapp.com>
2022-07-20 11:25:38 +0200

    Update dependencies & fix actions_api bugs

--- cortex_xdr_client/api/actions_api.py
+++ cortex_xdr_client/api/actions_api.py
@@ -17,7 +17,7 @@ class ActionsAPI(BaseAPI):
 
     # https://docs.paloaltonetworks.com/cortex/cortex-xdr/cortex-xdr-api/cortex-xdr-apis/response-actions/get-action-status.html
     def get_action_status(self,
-                   group_action_id: Optional[str]
+                   group_action_id: int
                    ) -> Optional[GetActionStatus]:
         """
         Retrieve the status of the requested actions according to the action ID.
@@ -25,7 +25,7 @@ class ActionsAPI(BaseAPI):
         :param group_action_id: String the represents the Action ID of the selected request.
         :return: Returns a GetActionStatus object if successful.
         """
-        request_data = new_request_data(other=group_action_id)
+        request_data = new_request_data(other={'group_action_id':group_action_id})
 
         response = self._call(call_name="get_action_status",
                               json_value=request_data)
