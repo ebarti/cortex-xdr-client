4dfb8962e014c4803bbd6c6c6368da029cfd2b9b
n1ls <lawiet47@gmail.com>
2023-06-12 10:45:31 +0200

    Fix unexpected NoneType in Actionstatus model (#77)

--- cortex_xdr_client/api/models/action_status.py
+++ cortex_xdr_client/api/models/action_status.py
@@ -1,11 +1,11 @@
-from typing import Dict, Optional
+from typing import Dict, Optional, Union
 
 from pydantic import BaseModel
 
 
 class ActionStatuStr(BaseModel):
     # Since we don't know what the returned key of <agent ID>/<endpoint ID> will be.
-    __root__: Dict[str, str]
+    __root__: Dict[Union[str, None], Union[str, None]]
 
 
 class GetActionStatusItem(BaseModel):
